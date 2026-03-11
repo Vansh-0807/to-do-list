a706d6a2ab28d5f76d5804d3cd4bbd4ecb1f81ec
Vansh-0807 <vansh.rateria15@gmail.com>
2026-03-12 02:40:24 +0530

    Made some changes

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('task_list/', views.task_list, name = 'task_list'),
+    path('', views.task_list, name = 'task_list'),
     path('add/', views.add_task, name = 'add_task'),
     path('complete/<int:task_id>/', views.complete_task, name = 'complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name = 'delete_task'),
